58e14d174ecce26cd6f50b67ec5e0d0c8cedd066
ChanuCsj <chanucsj@gmail.com>
2025-03-16 08:35:17 +0530

    Admin Payment UI Done

--- src/main/java/org/example/smarthotelbookingwebsite/controller/PaymentController.java
+++ src/main/java/org/example/smarthotelbookingwebsite/controller/PaymentController.java
@@ -9,7 +9,7 @@ import org.example.smarthotelbookingwebsite.util.VarList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/payment")
 public class PaymentController {
